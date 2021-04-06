3040a8db9553a3846a7bdb2ee9e4a658909f04dc
Anton <Reshetov190991@mail.ru>
2021-04-07 00:04:04 +0700

    Разобрал fetch

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('work app')
+const requestURL = 'https://jsonplaceholder.typicode.com/users'
+
+function sendRequest(method, url, body = null) {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {   
+        if(response.ok){
+            return response.json()    
+        }
+                                                        // Способ обработки ошибки
+        return response.json().then(error=>{
+            const error = new Error('Ошибка')
+            error.data = error
+            throw error
+        })
+    })
+}
+
+const body = {
+    name: 'Petr',
+    age: 20
+}
+
+// sendRequest('GET', requestURL)
+//     .then(data => console.log(data))
+///    .catch(error => console.log(error))
+
+sendRequest('POST', requestURL, body)
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
